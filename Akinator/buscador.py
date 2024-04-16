4364bb2c18c8bbe2fdcfe3fece5cdba1d6921b51
Daniel Hernandez <danielalejandro627@gmail.com>
2024-04-16 16:22:02 -0500

    Modificacion de reglas y taxonomia

--- Akinator/buscador.py
+++ Akinator/buscador.py
@@ -12,66 +12,57 @@ engine = pyttsx3.init()
 def es_consecuente(X, Y):
     # Verificar si Y es consecuente con X
     if nivel_1(X):
-        nivel_2(X, Y)
+        return nivel_1(X) == Y
     elif nivel_2(X):
-        nivel_3(X, Y)
+        return nivel_2(X) == Y
     elif nivel_3(X):
-        nivel_4(X, Y)
+        return nivel_3(X) == Y
     elif nivel_4(X):
-        nivel_5(X, Y)
+        return nivel_4(X) == Y
     elif nivel_5(X):
-        nivel_6(X, Y)
+        return nivel_5(X) == Y
     elif nivel_6(X):
-        nivel_7(X, Y)
+        return nivel_6(X) == Y
+    elif nivel_7(X):
+        return nivel_7(X) == Y
+    else:
+        return False
 
 def nivel_1(X):
     return X == 'bic_tec'
 
-def nivel_2(X, Y):
-    if X == 'bic_tec':
-        return Y == 'software' or Y == 'hardware'
+def nivel_2(X):
+    return X in taxonomia['bic_tec']
 
-def nivel_3(X, Y):
+def nivel_3(X):
     if X == 'software':
-        return Y == 'internet' or Y == 'so'
+        return X in taxonomia['bic_tec']['software']
     elif X == 'hardware':
-        return Y == 'dispositivos' or Y == 'componentes'
+        return X in taxonomia['bic_tec']['hardware']
 
-def nivel_4(X, Y):
-    if X == 'internet':
-        return Y == 'motor_de_busqueda' or Y == 'web2' or Y == 'web3'
-    elif X == 'so':
-        return Y == 'movil' or Y == 'escritorio'
+def nivel_4(X):
+    if X == 'internet' or X == 'so':
+        return X in taxonomia['bic_tec']['software']
+    elif X == 'dispositivos' or X == 'componentes':
+        return X in taxonomia['bic_tec']['hardware']
 
-def nivel_5(X, Y):
-    if X == 'motor_de_busqueda':
-        return Y == 'web2' or Y == 'web3'
-    elif X == 'movil':
-        return Y == 'Abierto' or Y == 'Cerrado'
-    elif X == 'escritorio':
-        return Y == 'abierto' or Y == 'cerrado'
+def nivel_5(X):
+    if X == 'motor_de_busqueda' or X == 'movil' or X == 'escritorio':
+        return X in taxonomia['bic_tec']['software']
+    elif X == 'abierto' or X == 'cerrado':
+        return X in taxonomia['bic_tec']['hardware']
 
-def nivel_6(X, Y):
-    if X == 'web2':
-        return Y == 'open_source'
-    elif X == 'web3':
-        return Y == 'Dapps' or Y == 'NFTs'
-    elif X == 'Abierto':
-        return Y == 'Android' or Y == 'Sailfish_Os'
-    elif X == 'Cerrado':
-        return Y == 'Android'
+def nivel_6(X):
+    if X == 'web2' or X == 'web3' or X == 'Abierto' or X == 'Cerrado':
+        return X in taxonomia['bic_tec']['software']
 
-def nivel_7(X, Y):
-    if X == 'open_source':
-        return Y == 'firefox' or Y == 'opera'
-    elif X == 'Dapps':
-        return Y == {}
-    elif X == 'NFTs':
-        return Y == {}
-    elif X == 'Android':
-        return Y == {}
-    elif X == 'Sailfish_Os':
-        return Y == {}
+def nivel_7(X):
+    if X == 'open_source' or X == 'Dapps' or X == 'NFTs' or X == 'Android' or X == 'Sailfish_Os':
+        return X in taxonomia['bic_tec']['software']
+    elif X == 'firefox' or X == 'opera':
+        return X in taxonomia['bic_tec']['software']['internet']['motor_de_busqueda']['web2']
+    else:
+        return False
 
 def inferir_elemento(caracteristicas, nodo_actual):
     if not caracteristicas:
@@ -85,26 +76,21 @@ def inferir_elemento(caracteristicas, nodo_actual):
             return None
 
     pregunta = next(iter(caracteristicas))
+    siguiente_nodo = caracteristicas[pregunta]
+
     mensaje = f"¿El elemento tiene la característica '{pregunta}'?"
     engine.say(mensaje)
     engine.runAndWait()
     respuesta = messagebox.askquestion("Akinator", mensaje)
 
     if respuesta == 'yes':
-        siguiente_nodo = caracteristicas[pregunta]
         return inferir_elemento(siguiente_nodo, pregunta)
     elif respuesta == 'no':
         caracteristicas_restantes = {carac: caracteristicas[carac] for carac in caracteristicas if carac != pregunta}
         if not caracteristicas_restantes:
             return None
 
-        for caracteristica in caracteristicas_restantes:
-            siguiente_nodo = caracteristicas_restantes[caracteristica]
-            resultado = inferir_elemento(siguiente_nodo, caracteristica)
-            if resultado:
-                return resultado
-        
-        return None
+        return inferir_elemento(caracteristicas_restantes, nodo_actual)
     else:
         messagebox.showerror("Error", "Respuesta no válida. Por favor responda 'yes' para sí o 'no' para no.")
         return inferir_elemento(caracteristicas, nodo_actual)
@@ -112,8 +98,9 @@ def inferir_elemento(caracteristicas, nodo_actual):
 def iniciar_adivinanza():
     # Iniciar la búsqueda desde el nivel 0 (bic_tec) del JSON de taxonomía
     resultado = inferir_elemento(taxonomia, 'bic_tec')
+
     if resultado:
-        mensaje_resultado = f"El elemento es '{resultado}'."
+        mensaje_resultado = f"¡He adivinado tu compañía! Es {resultado}."
         engine.say(mensaje_resultado)
         engine.runAndWait()
         messagebox.showinfo("Akinator", mensaje_resultado)
